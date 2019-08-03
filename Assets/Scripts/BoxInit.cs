93578fd73990e5164f3a3474280a21de81aca915
Yohey Funada <dev@fulufulu.com>
2019-08-03 21:22:09 +0900

    result scene add and been error result scene

--- Assets/Scripts/BoxInit.cs
+++ Assets/Scripts/BoxInit.cs
@@ -8,27 +8,29 @@ public class BoxInit : MonoBehaviour
     public GameObject boxObjPrefab;
     public GameObject boxesObj;
 
-    private void Awake()
+    void Awake()
     {
-        for (int x = 0;x < 8; x++)
+        GameObject masterObj = GameObject.Find("Master");  //<=====これ
+        for (int x = 0; x < 8; x++)
         {
             for (int y = 0; y < 5; y++)
             {
                 GameObject g = Instantiate(boxObjPrefab, boxesObj.transform);
                 g.transform.position = new Vector3((2f + (1f * y)), 0.4f, (-4.2f + (1.2f * x)));
+                g.GetComponent<Destroyer>().masterObj = masterObj;  //<=====これ
             }
         }
     }
 
-    // Start is called before the first frame update
+    // Use this for initialization
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
